d0ca60dd6c5b5ed87b02e2f5047cddfc5f321d11
Dhruv2311 <dhruv.sonagra.cg@gmail.com>
2025-03-22 18:30:11 +0530

    impliment userdashboard and profile section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route , useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -6,6 +6,10 @@ import ProductList from './components/Product/ProductList';
 import SingleProduct from './components/SingleProduct/SingleProduct';
 import FarmerProfile from './components/FarmerProfile/FarmerProfile';
 import Farmer from './components/Farmer';
+import Recommendation from './components/Recommendation';
+import UserDashboard from './UserDashboard';
+
+
 
 function App() {
   return (
@@ -18,20 +22,22 @@ function App() {
 function AppContent() {
   const location = useLocation();
   const isFarmerDashboard = location.pathname.startsWith("/farmerpanel");
-  
+
   return (
     <>
-       {!isFarmerDashboard && <Navbar />}
-      <Routes>         
-        <Route path="/" element={< LandingPage/>} />
+      {!isFarmerDashboard && <Navbar />}
+      <Routes>
+        <Route path="/" element={< LandingPage />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/product/:productId" element={<SingleProduct />} />
         <Route path="/product" element={<SingleProduct />} />
         <Route path="/farmer" element={<FarmerProfile />} />
         <Route path="/farmerpanel/*" element={<Farmer />} />
+        <Route path="/user/*" element={<UserDashboard />} />
+
       </Routes>
       <Footer />
-   </>
+    </>
   );
 }
 

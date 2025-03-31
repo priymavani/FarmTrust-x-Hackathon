951b60f1da4bef5c945e3d58fad757ea955ba387
PRIY MAVANI <priy.mavani.cg@gmail.com>
2025-03-31 10:42:50 +0530

    deployed frontend link in cors

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const PORT = 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://farmtrust.netlify.app'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
     credentials: true,
@@ -50,7 +50,7 @@ cloudinary.config({
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://farmtrust.netlify.app'],
   methods: ['GET', 'POST', 'PATCH'],
   credentials: true,
 }));
